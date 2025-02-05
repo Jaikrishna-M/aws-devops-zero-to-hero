db9dce41f11ac2dda5399d5d6806265ba2da5bf3
Jaikrishna-M <154756917+Jaikrishna-M@users.noreply.github.com>
2025-02-05 18:42:39 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 set -e
-
-# Name of the container to stop (replace with your container name)
-CONTAINER_NAME="my_app_container"
-
-# Check if the container is running
-RUNNING_CONTAINER=$(docker ps -q -f name=${CONTAINER_NAME})
-
-if [ ! -z "$RUNNING_CONTAINER" ]; then
-    echo "Stopping the running container: $CONTAINER_NAME"
-    docker stop $CONTAINER_NAME
-
-    echo "Removing the container: $CONTAINER_NAME"
-    docker rm $CONTAINER_NAME
-else
-    echo "No running container named '$CONTAINER_NAME' found."
-fi
+docker ps -a -q | xargs -r docker stop | xargs -r docker rm
